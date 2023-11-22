3f49aee879ed2c296acaa4538a6ddf9c6adc497e
jirilcs10 <jirilcherianshaju@gmail.com>
2023-11-22 11:51:41 +0530

    corrected

--- task9/app.js
+++ task9/app.js
@@ -3,6 +3,6 @@ const http=require('http');
 
 const route = require('./routes');
 // console.log(route.code);
-const server=http.createServer(route.handler);
+const server=http.createServer(route);
 
 server.listen(4000);
