a5b32cfd2bd6b6c08148f2118ef09a071a70881d
jirilcs10 <jirilcherianshaju@gmail.com>
2023-11-24 11:55:28 +0530

    exptask2

--- Expresstask1/app.js
+++ Expresstask1/app.js
@@ -1,14 +1,20 @@
 
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("Midlleware 1");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><label for='title'>Product</label><input type='text' name='title'></input><br></br><label for='size'>Size</label><input type='number' name='size'></input><button type='submit'>Add Product</button></form>")
 });
-app.use((req,res,next)=>{
-    console.log("Midlleware 2");
-    res.send('<h1>Express Page</h1>');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body.title);
+    console.log(req.body.size);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
+     res.send('<h1>Express Page</h1>')
 });
 
 
