b3fd0832b50bceff6cd93f012cddccc4b57a902e
jirilcs10 <jirilcherianshaju@gmail.com>
2023-11-27 10:23:39 +0530

    exptask3

---
+++ Expresstask1/app1.js
@@ -0,0 +1,23 @@
+
+const express=require('express');
+
+const bodyParser=require('body-parser');
+const app=express();
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Home Page</h1>');
+});
+app.use((req,res,next)=>{
+     res.status(404).send('<h1>Page Not Found</h1>');
+});
+
+
+app.listen(4000);
