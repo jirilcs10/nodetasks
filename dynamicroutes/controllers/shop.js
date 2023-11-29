02cc3405cb8c7b6f990189fa33a2aed7429dd5f7
jirilcs10 <jirilcherianshaju@gmail.com>
2023-11-29 18:16:57 +0530

    exptask9

--- dynamicroutes/controllers/shop.js
+++ dynamicroutes/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart=require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -11,7 +12,6 @@ exports.getProducts = (req, res, next) => {
 };
 exports.getProduct=(req,res,next)=>{
   const prodId=req.params.productId;
-  console.log(prodId);
   Product.findById(prodId,product=>{
     res.render('shop/product-detail',{
       product:product,
@@ -36,6 +36,13 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+exports.postCart = (req,res,next)=>{
+   const prodId=req.body.productId;
+   Product.findById(prodId,product=>{
+     Cart.addProduct(prodId,product.price);
+   });
+   res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
