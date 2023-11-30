b850ca28c65ef7fcf132e9aa244f1d634270f1b8
jirilcs10 <jirilcherianshaju@gmail.com>
2023-11-30 12:05:32 +0530

    exptask16

--- dynamicroutes/controllers/shop.js
+++ dynamicroutes/controllers/shop.js
@@ -2,33 +2,34 @@ const Product = require('../models/product');
 const Cart=require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+  Product.fetchAll().then(([rows])=>{
+    res.render('shop/product-list',{
+     prods:rows,
+     pageTitle:'Shop',
+     path:''
     });
-  });
+   }).catch(err=>console.log(err));
 };
 exports.getProduct=(req,res,next)=>{
   const prodId=req.params.productId;
-  Product.findById(prodId,product=>{
+  console.log(prodId);
+  Product.findById(prodId).then(([product])=>{
     res.render('shop/product-detail',{
-      product:product,
+      product:product[0],
       pageTitle:product.title,
       path:'/products'
     });
-  });
+  }).catch(err=>console.log(err));
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  Product.fetchAll().then(([rows,fieldData])=>{
+   res.render('shop/index',{
+    prods:rows,
+    pageTitle:'Shop',
+    path:'/'
+   });
+  }).catch(err=>console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
@@ -37,9 +38,11 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
 exports.postCart = (req,res,next)=>{
    const prodId=req.body.productId;
-   Product.findById(prodId,product=>{
+   console.log(prodId);
+   Product.findById(prodId,(product)=>{
      Cart.addProduct(prodId,product.price);
    });
    res.redirect('/cart');
